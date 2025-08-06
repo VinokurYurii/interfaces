06d6f2528c8a0c2a6e8ee5826c086c5dfcfa7a5f
Yurii <yurii.vynokur@sigma.software>
2025-08-06 20:41:09 +0300

    Try HTTP

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+type englishBot struct{}
+type spanishBot struct{}
+
+type bot interface {
+	getGreeting() string
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreating(eb)
+	printGreating(sb)
+
+	resp, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp)
+}
+
+func printGreating(b bot) {
+	println(b.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	return "Hello, World!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
 }
