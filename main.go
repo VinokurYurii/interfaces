6721e8b2f241599f4a77224b0e8661211e513593
Yurii <yurii.vynokur@sigma.software>
2025-08-07 01:02:51 +0300

    Implement own filereader

--- main.go
+++ main.go
@@ -1,41 +1,17 @@
 package main
 
-import "fmt"
-
-type triangle struct {
-	base   float64
-	height float64
-}
-
-type square struct {
-	sideLength float64
-}
-
-type shape interface {
-	area() float64
-}
-
-func (t triangle) area() float64 {
-	return 0.5 * t.base * t.height
-}
-
-func (s square) area() float64 {
-	return s.sideLength * s.sideLength
-}
-
-func printArea(s shape) {
-	fmt.Printf("Area is: %v", s.area())
-}
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-	t := triangle{
-		base:   12.5,
-		height: 12,
+	filename := os.Args[1]
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Printf("Error: %v", err.Error())
+		os.Exit(1)
 	}
-	s := square{
-		sideLength: 5.0,
-	}
-
-	printArea(t)
-	printArea(s)
+	io.Copy(os.Stdout, file)
 }
